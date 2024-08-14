91164666bbd2ca6ff2a4e5df26cd62d0efe98f5b
nlia656 <nlia656@aucklanduni.ac.nz>
2024-08-15 01:11:07 +1200

    added shape text and colour text to the puzzle cards

--- frontend/src/components/HackPuzzle.js
+++ frontend/src/components/HackPuzzle.js
@@ -10,6 +10,8 @@ const HackPuzzle  = () =>{
     const dummyCardColours = ["gray", "purple", "blue", "#067910"];
     const dummyShapeColours = ["green", "white", "black", "red"];
     const dummyShapeType = ["triangle", "rectangle", "square", "circle"];
+    const dummyShapeText = ["square", "triangle", "circle", "rectangle"];
+    const dummyColourText = ["red", "green", "blue", "purple"];
     
 
     return (
@@ -18,21 +20,29 @@ const HackPuzzle  = () =>{
                 cardColour={dummyCardColours[0]}
                 shapeColour={dummyShapeColours[0]}
                 shapeType={dummyShapeType[0]}
+                shapeText={dummyShapeText[0]}
+                colourText={dummyColourText[0]}
             />
             <PuzzleCard 
                 cardColour={dummyCardColours[1]}
                 shapeColour={dummyShapeColours[1]}
                 shapeType={dummyShapeType[1]}
+                shapeText={dummyShapeText[1]}
+                colourText={dummyColourText[1]}
             />
             <PuzzleCard 
                 cardColour={dummyCardColours[2]}
                 shapeColour={dummyShapeColours[2]}
                 shapeType={dummyShapeType[2]}
+                shapeText={dummyShapeText[2]}
+                colourText={dummyColourText[2]}
             />
             <PuzzleCard 
                 cardColour={dummyCardColours[3]}
                 shapeColour={dummyShapeColours[3]}
                 shapeType={dummyShapeType[3]}
+                shapeText={dummyShapeText[3]}
+                colourText={dummyColourText[3]}
             />
         </div>
     )
--- frontend/src/components/PuzzleCard.js
+++ frontend/src/components/PuzzleCard.js
@@ -1,11 +1,13 @@
 import React from "react";
 import './HackPuzzle.css';
 
-const PuzzleCard = ({cardColour, shapeColour, shapeType}) => {
+const PuzzleCard = ({cardColour, shapeColour, shapeType, shapeText, colourText}) => {
 
     return (
         <div className="puzzle-card" style={{backgroundColor: cardColour}}>
             <div className={shapeType} style={{borderBottomColor: shapeType == "triangle" ? shapeColour : "transparent", backgroundColor: shapeType != "triangle" ? shapeColour : "transparent"}}>
+                <div className="puzzle-text">{shapeText}</div>
+                <div className="puzzle-text">{colourText}</div>
             </div>
         </div>
 
