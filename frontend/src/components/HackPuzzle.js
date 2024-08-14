45be46d01cb2b148ccbf71082a20ea21cf1a6e10
nlia656 <nlia656@aucklanduni.ac.nz>
2024-08-14 20:12:53 +1200

    added circle, square and rectangle to the shapes

--- frontend/src/components/HackPuzzle.js
+++ frontend/src/components/HackPuzzle.js
@@ -6,8 +6,10 @@ const HackPuzzle  = () =>{
     const SHAPES = ["circle", "triangle", "square", "rectangle"]
     //const cards = []
     const COLOURS = ["white", "black", "red", "blue","green", "yellow", "pink"]
+    //these dummy arrays should be replaced once we have the ranomiser for these
     const dummyCardColours = ["gray", "purple", "blue", "#067910"];
     const dummyShapeColours = ["green", "white", "black", "red"];
+    const dummyShapeType = ["triangle", "rectangle", "square", "circle"];
     
 
     return (
@@ -15,18 +17,22 @@ const HackPuzzle  = () =>{
             <PuzzleCard 
                 cardColour={dummyCardColours[0]}
                 shapeColour={dummyShapeColours[0]}
+                shapeType={dummyShapeType[0]}
             />
             <PuzzleCard 
                 cardColour={dummyCardColours[1]}
                 shapeColour={dummyShapeColours[1]}
+                shapeType={dummyShapeType[1]}
             />
             <PuzzleCard 
                 cardColour={dummyCardColours[2]}
                 shapeColour={dummyShapeColours[2]}
+                shapeType={dummyShapeType[2]}
             />
             <PuzzleCard 
                 cardColour={dummyCardColours[3]}
                 shapeColour={dummyShapeColours[3]}
+                shapeType={dummyShapeType[3]}
             />
         </div>
     )
--- frontend/src/components/PuzzleCard.js
+++ frontend/src/components/PuzzleCard.js
@@ -1,11 +1,12 @@
 import React from "react";
 import './HackPuzzle.css';
 
-const PuzzleCard = ({cardColour, shapeColour}) => {
+const PuzzleCard = ({cardColour, shapeColour, shapeType}) => {
 
     return (
         <div className="puzzle-card" style={{backgroundColor: cardColour}}>
-            <div className="triangle" style={{borderBottomColor: shapeColour}}></div>
+            <div className={shapeType} style={{borderBottomColor: shapeType == "triangle" ? shapeColour : "transparent", backgroundColor: shapeType != "triangle" ? shapeColour : "transparent"}}>
+            </div>
         </div>
 
     )
