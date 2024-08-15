29fd0867874c4876d3ff41b3330619dd166cc9a7
nlia656 <nlia656@aucklanduni.ac.nz>
2024-08-15 14:59:35 +1200

    added numbers and number colour

--- frontend/src/components/HackPuzzle.js
+++ frontend/src/components/HackPuzzle.js
@@ -12,6 +12,8 @@ const HackPuzzle  = () =>{
     const dummyShapeType = ["triangle", "rectangle", "square", "circle"];
     const dummyShapeText = ["square", "triangle", "circle", "rectangle"];
     const dummyColourText = ["red", "green", "blue", "purple"];
+    const dummyNumber = ["1","2","3","4"];
+    const dummyNumberColours = ["purple", "yellow", "white", "blue"];
     
 
     return (
@@ -22,6 +24,8 @@ const HackPuzzle  = () =>{
                 shapeType={dummyShapeType[0]}
                 shapeText={dummyShapeText[0]}
                 colourText={dummyColourText[0]}
+                number={dummyNumber[0]}
+                numberColour={dummyNumberColours[0]}
             />
             <PuzzleCard 
                 cardColour={dummyCardColours[1]}
@@ -29,6 +33,8 @@ const HackPuzzle  = () =>{
                 shapeType={dummyShapeType[1]}
                 shapeText={dummyShapeText[1]}
                 colourText={dummyColourText[1]}
+                number={dummyNumber[1]}
+                numberColour={dummyNumberColours[1]}
             />
             <PuzzleCard 
                 cardColour={dummyCardColours[2]}
@@ -36,6 +42,8 @@ const HackPuzzle  = () =>{
                 shapeType={dummyShapeType[2]}
                 shapeText={dummyShapeText[2]}
                 colourText={dummyColourText[2]}
+                number={dummyNumber[2]}
+                numberColour={dummyNumberColours[2]}
             />
             <PuzzleCard 
                 cardColour={dummyCardColours[3]}
@@ -43,6 +51,8 @@ const HackPuzzle  = () =>{
                 shapeType={dummyShapeType[3]}
                 shapeText={dummyShapeText[3]}
                 colourText={dummyColourText[3]}
+                number={dummyNumber[3]}
+                numberColour={dummyNumberColours[3]}
             />
         </div>
     )
--- frontend/src/components/PuzzleCard.js
+++ frontend/src/components/PuzzleCard.js
@@ -1,12 +1,13 @@
 import React from "react";
 import './HackPuzzle.css';
 
-const PuzzleCard = ({cardColour, shapeColour, shapeType, shapeText, colourText}) => {
+const PuzzleCard = ({cardColour, shapeColour, shapeType, shapeText, colourText, number, numberColour}) => {
 
     return (
         <div className="puzzle-card" style={{backgroundColor: cardColour}}>
             <div className={shapeType} style={{borderBottomColor: shapeType == "triangle" ? shapeColour : "transparent", backgroundColor: shapeType != "triangle" ? shapeColour : "transparent"}}>
                 <div className="puzzle-text">{shapeText}</div>
+                <div className="number" style={{color: numberColour}}>{number}</div>
                 <div className="puzzle-text">{colourText}</div>
             </div>
         </div>
